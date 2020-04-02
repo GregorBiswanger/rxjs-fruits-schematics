abeca753a7908b4de2c510b53cd4c968861c04a1
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2020-04-02 02:10:36 +0200

    first updateAppRoutingModule function init code

--- src/exercise/index.ts
+++ src/exercise/index.ts
@@ -1,5 +1,8 @@
-import { Rule, SchematicContext, Tree, url, apply, move, mergeWith, MergeStrategy, template } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, url, apply, move, mergeWith, MergeStrategy, template, SchematicsException, chain } from '@angular-devkit/schematics';
 import { normalize, strings } from '@angular-devkit/core';
+import { WorkspaceSchema } from '@angular-devkit/core/src/experimental/workspace';
+import * as ts from 'typescript';
+import { SourceFile } from 'typescript';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -17,10 +20,58 @@ export function exercise(_options: Options): Rule {
     ]);
 
     const templateRule = mergeWith(exerciseTree, MergeStrategy.Default);
-    return templateRule(tree, _context);
+    const updateAppRoutingModuleRule = updateAppRoutingModule(_options);
+    const chainedRule = chain([templateRule, updateAppRoutingModuleRule]);
+
+    return chainedRule(tree, _context);
   };
 }
 
-interface Options {
-  name: string;
+// function createCypressIntegrationTest(_options: Options): Rule {
+
+// }
+
+function getWorkspace(_options: Options, tree: Tree): WorkspaceSchema {
+  const workspace = tree.read('/angular.json');
+
+  if (!workspace) {
+    throw new SchematicsException('angular.json file not found!');
+  }
+
+  return JSON.parse(workspace.toString());
 }
+
+function updateAppRoutingModule(_options: Options): Rule {
+  return (tree: Tree, _context: SchematicContext): Tree => {
+    const moduleName = strings.dasherize(_options.name);
+    const exportedModuleName = strings.classify(_options.name);
+    const pathName = strings.dasherize(_options.name);
+    const appRoutingModulePath = 'src/app/app-routing.module.ts';
+
+    const routeContent = `  { path: '${pathName}', loadChildren: () => import('./exercises/${pathName}/${moduleName}.module').then(m => m.${exportedModuleName}) }`;
+
+    const appRoutingModuleSourceFile = getAsSourceFile(tree, appRoutingModulePath);
+    
+
+
+      // const recorder = tree.beginUpdate(appRoutingModulePath);
+      // recorder.insertLeft(0, routeContent);
+      // tree.commitUpdate(recorder);
+
+      return tree;
+    }
+  }
+
+  function getAsSourceFile(tree: Tree, path: string): SourceFile {
+    const file = tree.read(path);
+
+    if (!file) {
+      throw new SchematicsException(`${path} not found.`);
+    }
+
+    return ts.createSourceFile(path, file.toString(), ts.ScriptTarget.Latest, true);
+  }
+
+  interface Options {
+    name: string;
+  }
